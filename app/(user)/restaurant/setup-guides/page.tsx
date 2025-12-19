3f1f7515dc63808924d34a26090622d2d7b8bead
meshackdaniel <meshackdaniel225@gmail.com>
2025-12-19 19:14:44 +0100

    Update Dashboard

--- app/(user)/restaurant/setup-guides/page.tsx
+++ app/(user)/restaurant/setup-guides/page.tsx
@@ -1,15 +1,14 @@
 "use client";
 
-import { CheckCircle2, Circle, CircleDashed } from "lucide-react";
+import { CheckCircle2, Circle, CircleDashed, X } from "lucide-react";
 import { Progress } from "@/components/ui/progress";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import Image from "next/image";
+import { use, useState } from "react";
 
 export default function SetupGuidePage() {
-  const progress = 25; // Update this value dynamically based on completion
-
   const tasks = [
     {
       title: "Add Store Details",
@@ -17,8 +16,7 @@ export default function SetupGuidePage() {
         "Provide your store name, address, hours and basic informations.",
       completed: true,
       actionLabel: "Add details",
-      actionVariant: "default" as const,
-      actionSize: "sm" as const,
+      href: "/restaurant/dashboard",
     },
     {
       title: "Complete KYC Verification",
@@ -26,17 +24,15 @@ export default function SetupGuidePage() {
         "Submit required documents so Munchspace can verify your business and unlock full features.",
       completed: true,
       actionLabel: "Begin KYC",
-      actionVariant: "default" as const,
-      actionSize: "default" as const,
+      href: "/restaurant/dashboard",
     },
     {
       title: "Add Settlement Account",
       description:
         "Provide the bank account where your earnings should be deposited. (Munchspace handles customer payments.)",
-      completed: false,
+      completed: true,
       actionLabel: "Add account",
-      actionVariant: "default" as const,
-      actionSize: "default" as const,
+      href: "/restaurant/dashboard",
     },
     {
       title: "Add 3+ Menu Items",
@@ -44,8 +40,7 @@ export default function SetupGuidePage() {
         "List at least three dishes or products with names, prices and photos so customers can order.",
       completed: false,
       actionLabel: "Add items",
-      actionVariant: "default" as const,
-      actionSize: "default" as const,
+      href: "/restaurant/dashboard",
     },
     {
       title: "Set Charges & Fees",
@@ -53,8 +48,7 @@ export default function SetupGuidePage() {
         "Define packaging or service fees so prices stay clear and accurate.",
       completed: false,
       actionLabel: "Add charges",
-      actionVariant: "default" as const,
-      actionSize: "default" as const,
+      href: "/restaurant/dashboard",
     },
     {
       title: "Add Team Members",
@@ -62,11 +56,22 @@ export default function SetupGuidePage() {
         "Invite your staff members to manage orders, menus and operations.",
       completed: false,
       actionLabel: "Add team members",
-      actionVariant: "default" as const,
-      actionSize: "default" as const,
+      href: "/restaurant/dashboard",
     },
   ];
 
+  let [modifiedTasks, setModifiedTasks] = useState(tasks);
+
+  const progress = Number(
+    (
+      (tasks.filter((task) => task.completed).length / tasks.length) *
+      100
+    ).toFixed(0)
+  );
+
+  function deleteTaskByTitle(title: string) {
+    setModifiedTasks(modifiedTasks.filter((task) => task.title !== title));
+  }
   return (
     <div className="min-h-screen">
       <div className="px-5 md:px-15 py-12 mt-5 md:mt-0">
@@ -99,10 +104,10 @@ export default function SetupGuidePage() {
 
         {/* Task Grid */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          {tasks.map((task, index) => (
+          {modifiedTasks.map((task, index) => (
             <div
               key={index}
-              className="bg-gray-50 text-black rounded-2xl p-6 flex flex-col justify-between border"
+              className="text-black rounded-2xl p-6 flex flex-col justify-between border border-gray-100"
             >
               <div className="flex items-start gap-3">
                 {task.completed ? (
@@ -123,17 +128,34 @@ export default function SetupGuidePage() {
                 </div>
               </div>
               <div className="mt-6">
-                <Button
-                  variant={task.actionVariant}
-                  size={task.actionSize}
-                  className={cn(
-                    "rounded-full",
-                    task.actionVariant === "default" &&
-                      "bg-munchprimary hover:bg-orange-600 text-white"
-                  )}
-                >
-                  {task.actionLabel}
-                </Button>
+                {!task.completed ? (
+                  <Link href={task.href}>
+                    <Button
+                      variant={task.completed ? "outline" : "default"}
+                      className="rounded-full bg-munchprimary hover:bg-orange-600 text-white"
+                    >
+                      <span>{task.actionLabel}</span>
+                    </Button>
+                  </Link>
+                ) : (
+                  <Button
+                    variant={task.completed ? "outline" : "default"}
+                    className={cn(
+                      "rounded-full",
+                      task.completed
+                        ? " bg-gray-100 border-gray-100"
+                        : "bg-munchprimary hover:bg-orange-600 text-white"
+                    )}
+                  >
+                    <div
+                      onClick={() => deleteTaskByTitle(task.title)}
+                      className="flex items-center gap-2 text-slate-700"
+                    >
+                      <X />
+                      <span>Dismiss</span>
+                    </div>
+                  </Button>
+                )}
               </div>
             </div>
           ))}
