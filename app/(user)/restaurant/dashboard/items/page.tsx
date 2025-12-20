26cb748c5edbafde640f9973655fe31f11fe098b
meshackdaniel <meshackdaniel225@gmail.com>
2025-12-20 14:08:59 +0100

    Update Dashboard items

--- app/(user)/restaurant/dashboard/items/page.tsx
+++ app/(user)/restaurant/dashboard/items/page.tsx
@@ -262,7 +262,6 @@ export default function BestSellingItemsPage() {
                 </div>
               ))}
             </div>
-
             <Button
               variant="ghost"
               size="icon"
